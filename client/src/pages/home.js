08bcefb2dbc01437613add17b38a1527bb7401de
Vanessa Orellana <vorellana814@gmail.com>
2021-02-11 22:26:39 -0500

    adding parallax

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import Typical from 'react-typical';
 // import { Link } from 'react-router-dom';
 import './style.css';
 
 
 function Home() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+  
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const renderContent = () => (
+    <>
+      <div className="Parallax__content__heading">
+        <h1 className="Parallax__content__heading__text">Closure</h1>
+        <h2 className="Parallax__content__heading__caption">
+          Your one-stop source of Web Development tricks
+        </h2>
+      </div>
+      <div className="Parallax__content__cta">
+        <p>
+          <b>1. Like the video.</b> Because it helps me and my channel
+        </p>
+        <p>
+          <b>2. Like the video.</b> To see more content like that!
+        </p>
+        <p>
+          <b>3. Follow the Github link.</b> And play with this code yourself!
+        </p>
+      </div>
+    </>
+  );
+
   return (
-    <div className="homeContainer">
-     Hello
-    </div>
-  )
-}
+    <section className="Parallax">
+      <div
+        className="homeContainer"
+        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
+      />
+      <div
+        className="Parallax__background-triangles"
+        style={{ transform: `translateY(${offsetY * 0.8}px)` }}
+      />
+      <div className="Parallax__content">{renderContent()}</div>
+    </section>
+  );
+};
 
 export default Home;
