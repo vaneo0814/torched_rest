841a5d02696614c963f0f72be284fa39788532b3
Vanessa Orellana <vorellana814@gmail.com>
2021-02-12 22:53:43 -0500

    finishing homepage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,54 +1,84 @@
 import React, { useState, useEffect } from "react";
+import { Parallax } from 'react-parallax';
 // import Typical from 'react-typical';
 // import { Link } from 'react-router-dom';
 import './style.css';
 
 
+const image1 =
+  "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
+const image2 =
+  "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
+const image3 =
+  "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg";
+const image4 =
+  "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg";
+
+const inlineStyle = {
+  background: '#fff',
+  left: '50%',
+  top: '50%',
+  position: 'absolute',
+  padding: '20px',
+  transform: 'translate(-50%, -50%)',
+}
+
+
 function Home() {
-  const [offsetY, setOffsetY] = useState(0);
-  const handleScroll = () => setOffsetY(window.pageYOffset);
-  
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const renderContent = () => (
-    <>
-      <div className="backgroundContainer">
-        <h1 className="Parallax__content__heading__text">Closure</h1>
-        <h2 className="Parallax__content__heading__caption">
-          Your one-stop source of Web Development tricks
-        </h2>
-      </div>
-      <div className="Parallax__content__cta">
-        <p>
-          <b>1. Like the video.</b> Because it helps me and my channel
-        </p>
-        <p>
-          <b>2. Like the video.</b> To see more content like that!
-        </p>
-        <p>
-          <b>3. Follow the Github link.</b> And play with this code yourself!
-        </p>
-      </div>
-    </>
-  );
-
   return (
-    <section className="Parallax">
-      <div
-        className="homeContainer"
-        style={{ transform: `translateY(-${offsetY * 0.5}px)` }}
-      />
-      <div
-        className="Parallax__background-triangles"
-        style={{ transform: `translateY(${offsetY * 0.8}px)` }}
-      />
-      <div className="Parallax__content">{renderContent()}</div>
-    </section>
+    <div className="homeContainer">
+      <Parallax className="frontContainer" strength={500}>
+        <div style={{ height: 1000 }}>
+
+        </div>
+      </Parallax>
+      <Parallax className="middleContainer" strength={-200} >
+        <div style={{ height: 500 }}>
+          <div className="row">
+            <div className="col leftCol">
+              <h1 className="leftHeader">
+                Serving you a little bit<br/>of everything.
+              </h1>
+              <hr/>
+              <p>
+              Resident brings chefs from the world’s top restaurants to host intimate dinners in unique, luxury spaces.
+              Reserve your seats below or book a private event by emailing hi@meetresident.com. We can't wait to host you!
+              </p>
+            </div>
+            <div className="col">
+              <img className="rightCol" src="https://i.postimg.cc/rsqqnD85/IMG-0747.jpg" width="900" alt="food">
+              </img>
+            </div>
+          </div>
+        </div>
+      </Parallax>
+      <h1>| | |</h1>
+      <Parallax
+        className="lastContainer"
+        strength={200}
+        renderLayer={percentage => (
+          <div
+            style={{
+              position: 'absolute',
+              width: '100px',
+              height: '100px',
+              borderRadius: '50%',
+              background: `rgba(255, 123, 23, ${percentage * 1})`,
+              left: '50%',
+              top: '50%',
+              transform: `translate(-50%, -50%) scale(${percentage * 5})`,
+            }}
+          >
+          </div>
+        )}
+      >
+        <div style={{ height: 500 }}>
+          <div className="menuBtn">Menu</div>
+        </div>
+      </Parallax>
+      <div style={{ height: '100vh' }}></div>
+    </div>
   );
-};
+}
 
 export default Home;
