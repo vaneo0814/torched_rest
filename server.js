767821424e61de3d7240c154dbfee100e0346358
Lohelani <lohelani.hicks@gmail.com>
2021-03-04 11:10:42 -0500

    heroku attempt 9million

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//might be causing atlas issue?
 //require("dotenv").config();
 
 // Define middleware here
